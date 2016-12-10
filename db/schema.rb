47458932d611552fb45b903d484fa962acb2e1f2
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2016-12-10 19:13:08 +0100

    Added devise authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210175418) do
+ActiveRecord::Schema.define(version: 20161210180657) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
